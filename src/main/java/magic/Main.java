a2dd0ac65f64ea8e59d8bbb2c394e10f4565435c
mikera <mike.r.anderson.13@gmail.com>
2015-07-28 09:35:02 +0800

    Move main method to right place :-)

--- src/main/java/magic/Main.java
+++ src/main/java/magic/Main.java
@@ -2,4 +2,7 @@ package magic;
 
 public class Main {
 
+	public static void main(String... args) {
+		System.out.println("Magic! 0.0.1");
+	}
 }
--- src/main/java/magic/RT.java
+++ src/main/java/magic/RT.java
@@ -2,7 +2,4 @@ package magic;
 
 public class RT {
 
-	public static void main(String... args) {
-		System.out.println("Magic! 0.0.1");
-	}
 }
