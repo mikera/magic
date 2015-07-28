16cdb9bde6ea3ba841ce8d790c268182d67b5f18
mikera <mike.r.anderson.13@gmail.com>
2015-07-28 11:12:21 +0800

    Predicate test

--- src/test/java/magic/lang/TestPredicates.java
+++ src/test/java/magic/lang/TestPredicates.java
@@ -4,10 +4,26 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import magic.fn.AFn1;
+import java.util.function.Predicate;
+
 public class TestPredicates {
 
 	@Test public void testPredicate() {
-		java.util.function.Predicate<Long> odd = (Long d) -> ((d&1L)==1L);
+		java.util.function.Predicate<Long> odd = (Long v) -> ((v&1L)==1L);
+		
+		assertEquals(true,odd.test(3L));
+		assertEquals(false,odd.test(4L));
+	}
+	
+	@Test public void testAFn1() {
+		Predicate<Object> odd = new AFn1<Object,Boolean>() {
+			@Override
+			public Boolean apply(Object a) {
+				Long v=(Long)a;
+				return ((v&1L)==1L);
+			}	
+		};
 		
 		assertEquals(true,odd.test(3L));
 		assertEquals(false,odd.test(4L));
