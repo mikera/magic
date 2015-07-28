8357f0cbf0c4667ebdfe85df3ff3811256e28501
mikera <mike.r.anderson.13@gmail.com>
2015-07-28 11:29:02 +0800

    Docs

--- src/main/java/magic/RT.java
+++ src/main/java/magic/RT.java
@@ -1,7 +1,18 @@
 package magic;
 
+/**
+ * Static class to support the Clojure runtime
+ * 
+ * @author Mike
+ */
 public class RT {
 
+	/**
+	 * Converts an object to a boolean primitive value, according to Clojure's truthiness rules
+	 * i.e. nil or Boolean.FALSE is false, everything else is truthy
+	 * @param o
+	 * @return
+	 */
 	public static boolean bool(Object o) {
 		if (o == Boolean.FALSE) return false;
 		return (o!=null);
