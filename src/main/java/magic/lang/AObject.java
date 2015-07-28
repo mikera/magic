f23fb08689938258e0ff3e2def25b53492ea822b
mikera <mike.r.anderson.13@gmail.com>
2015-07-28 11:45:36 +0800

    Basic AObject implementations

--- src/main/java/magic/lang/AObject.java
+++ src/main/java/magic/lang/AObject.java
@@ -1,10 +1,29 @@
 package magic.lang;
 
+import java.io.Serializable;
+
 /**
  * Abstract base class for Magic Objects
  * @author Mike
  *
  */
-public class AObject {
+public abstract class AObject implements Cloneable, Serializable {
 	
+	public abstract long hash();
+
+	public abstract long hash(long seed);
+	
+	public int hashCode() {
+		long hash=hash();
+		return ((int)hash)^((int)(hash>>>32));
+	}
+	
+	public AObject clone() {
+		try {
+			return (AObject) super.clone();
+		} catch (CloneNotSupportedException e) {
+			Tools.sneakyThrow(e);
+			return null;
+		}		
+	}
 }
