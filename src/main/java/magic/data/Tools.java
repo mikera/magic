54a0b56dc3e6b03bc325a45850fcb244a7f23f61
mikera <mike.r.anderson.13@gmail.com>
2017-04-20 10:12:42 +0800

    Fix seq for empty iterators

--- src/main/java/magic/data/Tools.java
+++ src/main/java/magic/data/Tools.java
@@ -8,10 +8,12 @@ import magic.data.impl.ListIndexSeq;
 public class Tools {
 
 	public static <T> ISeq<T> seq(Iterator<T> iterator) {
+		if (!iterator.hasNext()) return null;
 		ArrayList<T> list=new ArrayList<T>();
-		while(iterator.hasNext()) {
+		do {
 			list.add(iterator.next());
-		}
+		} while (iterator.hasNext());
+
 		return new ListIndexSeq<T>(list);
 	}
 
