d83d8ef6e4cf4c824cfc9390dad119f794d8f1e6
Mike <mike@mikera.net>
2017-06-08 11:00:24 +0100

    Misc edits

--- src/main/java/magic/fn/IFn1.java
+++ src/main/java/magic/fn/IFn1.java
@@ -1,5 +1,12 @@
 package magic.fn;
 
+/**
+ * 
+ * @author Mike
+ *
+ * @param <T> The type of inputs to the function
+ * @param <R> The return type of the function
+ */
 @FunctionalInterface
 public interface IFn1<T,R> extends IFixedFn<R> {
 
