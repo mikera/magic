56c25e5dbb0553e79250fa804d40d47420132af3
Mike <mike@mikera.net>
2017-07-01 15:38:41 +0100

    Fix main method

--- src/main/java/magic/Main.java
+++ src/main/java/magic/Main.java
@@ -15,12 +15,16 @@ public class Main {
 	private static final Context MAIN_CONTEXT=Core.INITIAL_CONTEXT;
 	
 	public static void main(String... args) throws FileNotFoundException {
-		if (args.length>0) {
-			String s=RT.getResourceAsString(args[0]);
-			evaluate(MAIN_CONTEXT,s);
-		} else {
+		if (args.length==0||(args[0].equals("help"))) {
 			System.out.println("Magic! 0.0.1");
+			System.out.println("Optional arguments:");
+			System.out.println("   help          => Display this help message");
+			System.out.println("   <filename>    => Execute a magic .mag script with the gievn filename");
+			System.out.println("   repl          => Launch a standard Magic REPL (TODO)");
+			System.exit(0);
 		}
+		String s=RT.getResourceAsString(args[0]);
+		evaluate(MAIN_CONTEXT,s);
 	}
 
 	private static EvalResult<?> evaluate(Context c, String s) {
