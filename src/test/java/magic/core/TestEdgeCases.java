f12a182175ca7a5735d6e6958948f3b5010641a4
Mike <mike@mikera.net>
2017-07-09 08:35:28 +0100

    Test for unquote in parameter vector

--- src/test/java/magic/core/TestEdgeCases.java
+++ src/test/java/magic/core/TestEdgeCases.java
@@ -17,4 +17,9 @@ public class TestEdgeCases {
 		assertEquals((Long)2L,exec("(def ~'a 2) a"));
 		assertEquals((Long)2L,exec("(def ~(symbol \"a\") 2) a"));
 	}
+	
+	@Test public void testArgUnquote() {
+		// unquote should create a symbol that can be used in arg vector
+		assertEquals((Long)3L,exec("(defn foo [~'a] (+ 2 a)) (foo 1)"));
+	}
 }
